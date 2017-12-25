a59411278b094b253a7ed68e8da5f4299aafeb58
music94 <avd04494@gmail.com>
2017-12-25 14:56:36 +0300

    Added landing page.

--- shopping/src/main/java/com/shop/controller/PageController.java
+++ shopping/src/main/java/com/shop/controller/PageController.java
@@ -1,7 +1,9 @@
 package com.shop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -10,7 +12,30 @@ public class PageController {
 	@RequestMapping(value= {"/", "/home", "/index"})
 	public ModelAndView index() {
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("hello", "Hello world!");
+		mv.addObject("greeting", "Hello world!");
 		return mv;
 	}
+	
+	@RequestMapping(value="/test")
+	public ModelAndView test(@RequestParam(value="greeting", required=false) String greeting) {
+		ModelAndView mv = new ModelAndView("page");
+		if(greeting == null) {
+			greeting = "Hello!";
+		}
+		mv.addObject("greeting", greeting);
+		return mv;
+	}
+	
+	
+
+	@RequestMapping(value="/test2/{greeting}")
+	public ModelAndView test2(@PathVariable("greeting")String greeting) {
+		ModelAndView mv = new ModelAndView("page");
+		if(greeting == null) {
+			greeting = "Hello!";
+		}
+		mv.addObject("greeting", greeting);
+		return mv;
+	}
+
 }
